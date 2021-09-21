a26f7977fc4960e9aeea23b462ef1fdafbb855b1
nahailey <nickk083@gmail.com>
2021-09-21 15:46:54 -0400

    these are the updates for edits and for the Card service test

--- rev_overcharge_sts/src/main/java/com/revature/overcharge/beans/TechTag.java
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/beans/TechTag.java
@@ -33,6 +33,12 @@ public class TechTag {
     private Set<Deck> decks = new HashSet<>();
 	
 
+	@Override
+	public String toString() {
+		return "TechTag [id=" + id + ", tag=" + tag + "]";
+	}
+
+
 	public TechTag() {
 		super();
 	}
