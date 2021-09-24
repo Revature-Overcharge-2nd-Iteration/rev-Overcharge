48e2800e47d39f77c83c13233debccb24fa24b9a
SantanaCh <santachill@yahoo.com>
2021-09-23 21:13:57 -0400

    MVP done, testing needed

--- rev_overcharge_sts/src/main/java/com/revature/overcharge/services/CardServiceImpl.java
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/services/CardServiceImpl.java
@@ -22,10 +22,10 @@ public class CardServiceImpl implements CardService {
 
     @Autowired
     CardRepo cr;
-    
+
     @Autowired
     DeckRepo dr;
-
+    
     @Autowired
     ObjectiveService os;
     
@@ -38,8 +38,11 @@ public class CardServiceImpl implements CardService {
             log.warn("Card id is invalid for add");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         } else {
-            c.setDeck(ds.getDeck(deckId));
+        	Deck d = ds.getDeck(deckId);
+            c.setDeck(d);
             c.setCreatedOn(new Date().getTime());
+            d.setStatus(1);
+            dr.save(d);
             c = cr.save(c);
             os.setAdd4CardsDaily(deckId, c);
             return c;
@@ -57,33 +60,17 @@ public class CardServiceImpl implements CardService {
     }
 
     @Override
-	public Card updateCard(int deckId, Card newCard) {
-		log.trace("updateCard(): newCard: [" + newCard.toString() + "]");
-
-		if (dr.existsById(deckId)) {
-			if (cr.existsById(newCard.getId())) {
-				
-				
-				Deck objDeck = ds.getDeck(deckId);
-				log.debug("updateCard(): retrieved objDeck: [" + objDeck.toString() + "]");
-				
-				newCard.setDeck(ds.getDeck(deckId));
-				
-				log.debug("updateCard(): cr.existsById(newCard.getId() is true: calling cr.save,");
-				Card objNewCard = cr.save(newCard);
-
-				log.debug("updateCard(): retrun from CRUD objNewCard: [" + objNewCard.toString() + "]");
-				return objNewCard;
-			} else {
-				log.warn("Card id is invalid for update");
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-			}
-		}else {
-			log.debug("updateCard(): NO deck found for this card by deck id: [" + deckId + "]");
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-		}
-		
-	}
+    public Card updateCard(Card newCard) {
+        if (cr.existsById(newCard.getId())) {
+        	Deck d = ds.getDeck(newCard.getDeck().getId());
+            d.setStatus(1);
+            dr.save(d);
+            return cr.save(newCard);
+        } else {
+            log.warn("Card id is invalid for update");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @Override
     public boolean deleteCard(int id) {
