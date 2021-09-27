8d9ac8dcef6ba80ba2fd436fd73965f39d3fe3af
McCabe Sommers <mccabe.sommers@gmail.com>
2021-09-27 15:04:31 -0700

    more working selenium tests! timer ones specifically

--- rev_overcharge_e2e/src/test/java/com/revature/overcharge/steps/TimerSteps.java
+++ rev_overcharge_e2e/src/test/java/com/revature/overcharge/steps/TimerSteps.java
@@ -1,5 +1,8 @@
 package com.revature.overcharge.steps;
 
+import org.openqa.selenium.JavascriptExecutor;
+
+
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.Assert.*;
 
@@ -22,21 +25,43 @@ public class TimerSteps {
 	private static WebDriver driver = TimerRunner.driver;
 	private static WebDriverWait wait = new WebDriverWait(driver, 10);
 	private static TimerWidget timer = TimerRunner.timer;
+	
+	
+
+	
 
     @Given("^User has navigated to the website for timer$")
     public void user_has_navigated_to_the_website_for_timer() throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+
     	timer.navigateTo(timer.getURL());
     	Thread.sleep(2000);
     }
     
     @Given("^User shows Timer$")
     public void user_shows_timer() throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	timer.timerLink.click();
     	wait.withTimeout(Duration.ofSeconds(2));
     }
     
     @Given("^Timer visibility is \"([^\"]*)\"$")
     public void timer_visibility_is_something(String visibility) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	String css = "";
     	switch (visibility) {
 		case "On":
@@ -54,6 +79,12 @@ public class TimerSteps {
 
     @When("^User clicks \"([^\"]*)\" on timer$")
     public void user_clicks_something_on_timer(String button) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	switch (button) {
 		case "Timer":
 			timer.timerLink.click();
@@ -89,6 +120,12 @@ public class TimerSteps {
 
     @When("^Timer display is \"([^\"]*)\"$")
     public void timer_display_is_something(String display) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	user_clicks_something_on_timer("Display Input");
     	Thread.sleep(2000);
     	user_inputs_and("0", "1");
@@ -101,6 +138,12 @@ public class TimerSteps {
 
     @Then("^Timer toggles visibility to \"([^\"]*)\"$")
     public void timer_toggles_visibility_to_something(String visibility) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	String css = "";
     	switch (visibility) {
 		case "On":
@@ -117,23 +160,47 @@ public class TimerSteps {
 
     @Then("^Timer status is \"([^\"]*)\"$")
     public void timer_status_is_something(String status) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	assertEquals(": "+status, timer.status.getText());
     }
 
     @Then("^Timer mode changes to \"([^\"]*)\"$")
     public void timer_mode_changes_to_something(String finalMode) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	ExpectedConditions.attributeToBe(timer.mode, "innerText", finalMode);
     	Thread.sleep(2000);
     }
 
     @Then("^Timer is set to (.+)$")
     public void timer_is_set_to(String newTime) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	ExpectedConditions.attributeToBe(timer.display, "innerText", newTime);
     	Thread.sleep(2000);
     }
 
     @And("^User inputs (.+) and (.+)$")
     public void user_inputs_and(String hours, String minutes) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	timer.hoursInput.sendKeys(hours);
     	Thread.sleep(2000);
     	timer.minInput.sendKeys(minutes);
@@ -143,7 +210,13 @@ public class TimerSteps {
     }
 
     @And("^Timer mode is \"([^\"]*)\"$")
-    public void timer_mode_is_something(String initMode) throws Throwable {    	
+    public void timer_mode_is_something(String initMode) throws Throwable {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;  
+		   js.executeScript("window.scrollBy(0,350)", "");
+		
+		Thread.sleep(2000);
+		
     	if (initMode == "Break") user_clicks_something_on_timer("Change Mode");
     	ExpectedConditions.attributeToBe(timer.mode, "innerText", initMode);
     	wait.withTimeout(Duration.ofSeconds(1));
